8e462c9e517a9b0ed66d3baccd40959450a86546
Alonso Avotz <alonso@avotz.com>
2017-06-20 14:51:20 -0600

    se quito dos request de js en functions

--- wp-content/themes/sibutours/functions.php
+++ wp-content/themes/sibutours/functions.php
@@ -111,9 +111,9 @@ add_action( 'widgets_init', 'sibutours_widgets_init' );
 function sibutours_scripts() {
 	wp_enqueue_style( 'sibutours-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'sibutours-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	//wp_enqueue_script( 'sibutours-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'sibutours-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'sibutours-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
