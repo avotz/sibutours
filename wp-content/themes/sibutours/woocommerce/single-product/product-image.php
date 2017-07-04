f3f1a27be35c810030fafe6cf2ab6d1844b6ea99
Alonso Avotz <alonso@avotz.com>
2017-07-04 11:46:59 -0600

    se actualizor archivo image galler woocommerce

--- wp-content/themes/sibutours/woocommerce/single-product/product-image.php
+++ wp-content/themes/sibutours/woocommerce/single-product/product-image.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.0.2
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,10 +22,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post, $product;
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
+$thumbnail_size    = apply_filters( 'woocommerce_product_thumbnails_large_size', 'tour-gallery' );
 $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
-$full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, 'tour-gallery' );
+$full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, $thumbnail_size );
 $thumb   = wp_get_attachment_image_src( $post_thumbnail_id, 'large' );
-$image_title       = get_post_field( 'post_excerpt', $post_thumbnail_id );
 $placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
 $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
 	'woocommerce-product-gallery',
