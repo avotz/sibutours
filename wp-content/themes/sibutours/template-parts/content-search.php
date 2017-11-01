f8d53feb6e4f630d97e6ae88e0f97de973f13820
Alonso Avotz <alonso@avotz.com>
2017-10-31 18:36:04 -0600

    fix search se puso la foto

--- wp-content/themes/sibutours/template-parts/content-search.php
+++ wp-content/themes/sibutours/template-parts/content-search.php
@@ -13,7 +13,33 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		<?php 
+		 if ( has_post_thumbnail() ) :
+
+          $id = get_post_thumbnail_id($post->ID);
+          $big_url = wp_get_attachment_image_src($id,'tour-gallery', true);
+          $thumb_url = wp_get_attachment_image_src($id,'large', true);
+          ?>
+          
+      	<?php  if(wp_is_mobile()){ ?>
+       		<figure class="post-banner blog-banner" style="background-image: url('<?php echo $thumb_url[0] ?>');">
+				<?php  if ( !is_single() ) :?>
+					<a href="<?php echo get_permalink() ?>"></a>
+				
+				<?php  endif;?>
+			</figure>
+
+		<?php  }else{ ?>
+			<figure class="post-banner blog-banner" style="background-image: url('<?php echo $big_url[0] ?>');">
+				<?php  if ( !is_single() ) :?>
+					<a href="<?php echo get_permalink() ?>"></a>
+				
+				<?php  endif;?>
+			</figure>
+         <?php } ?>
+
+      <?php endif; 
+		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php sibutours_posted_on(); ?>
 		</div><!-- .entry-meta -->
